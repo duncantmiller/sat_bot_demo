f00408a96e80ae687a7d262602dfe85f102e3805
Duncan Miller <dmiller@heatspring.com>
2024-01-10 11:04:58 -0800

    change test to assert openai object returned

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import unittest
 from question_builder import QuestionBuilder
+from openai.types.chat.chat_completion import ChatCompletion
 
 class TestQuestionBuilder(unittest.TestCase):
 
     def test_generate(self):
         question_builder = QuestionBuilder()
-        self.assertTrue(question_builder.generate())
+        response = question_builder.generate()
+        self.assertIsInstance(response, ChatCompletion)
 
 if __name__ == "__main__":
     unittest.main()
