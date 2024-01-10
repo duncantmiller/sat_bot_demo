19c0469b04304f8a238c103ea050d956fcc70135
Duncan Miller <dmiller@heatspring.com>
2024-01-10 11:56:51 -0800

    add a test to make sure response is valid json

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import unittest
 from question_builder import QuestionBuilder
 from openai.types.chat.chat_completion import ChatCompletion
+import json
 
 class TestQuestionBuilder(unittest.TestCase):
     def setUp(self):
@@ -9,6 +10,10 @@ class TestQuestionBuilder(unittest.TestCase):
 
     def test_generate(self):
         self.assertIsInstance(self.response, ChatCompletion)
+
+    def test_is_valid_json(self):
+        json_string = self.response.choices[0].message.content
+        self.assertTrue(json.loads(json_string), "should be valid json format")
 
 if __name__ == "__main__":
     unittest.main()
