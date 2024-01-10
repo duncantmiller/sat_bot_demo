97e64839744ba279e4d4573ef8c9a94d8e85f7bf
Duncan Miller <dmiller@heatspring.com>
2024-01-10 11:51:51 -0800

    move question builder initialization into setup method

--- test.py
+++ test.py
@@ -3,11 +3,12 @@ from question_builder import QuestionBuilder
 from openai.types.chat.chat_completion import ChatCompletion
 
 class TestQuestionBuilder(unittest.TestCase):
+    def setUp(self):
+        question_builder = QuestionBuilder()
+        self.response = question_builder.generate()
 
     def test_generate(self):
-        question_builder = QuestionBuilder()
-        response = question_builder.generate()
-        self.assertIsInstance(response, ChatCompletion)
+        self.assertIsInstance(self.response, ChatCompletion)
 
 if __name__ == "__main__":
     unittest.main()
