b3db8841370d5984d27794baae1f9163b284de40
Duncan Miller <dmiller@heatspring.com>
2024-01-10 09:34:54 -0800

    adding test file with basic test

---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestQuestionBuilder(unittest.TestCase):
+
+    def test_generate(self):
+        question_builder = QuestionBuilder()
+        self.assertTrue(question_builder.generate())
+
+if __name__ == "__main__":
+    unittest.main()
